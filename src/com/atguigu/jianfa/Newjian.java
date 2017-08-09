bad8d8df9af251552d845316fd2557f003565314
令狐冲 <linghuchong98961@126.com>
2017-08-10 00:23:48 +0800

    新剑法

---
+++ src/com/atguigu/jianfa/Newjian.java
@@ -0,0 +1,17 @@
+package com.atguigu.jianfa;
+/**  
+ *
+ *  
+ *@author lizhentao
+ *@version 
+ *@datetime 2017年8月10日上午12:20:43  
+ */
+
+public class Newjian {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("新剑法");
+	}
+
+}
